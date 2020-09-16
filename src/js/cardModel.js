1765ff212babaad945cf64f6b4e95ee7a4e048aa
hdkid7 <hameddiakite34@yahoo.com>
2020-09-16 01:20:56 -0400

    official changes

--- src/js/cardModel.js
+++ src/js/cardModel.js
@@ -4,7 +4,12 @@ const createCardModel = (cardObj) => {
     const element = `
     
     <div class="Undergraduate-Admissions-Profile-Card-Snippet-CardWrapper">
-                    <div class="Undergraduate-Admissions-Profile-Card-Snippet-Card" style="background-image:url('${cardObj.imageUrl}')">
+                    <div class="Undergraduate-Admissions-Profile-Card-Snippet-Card" >
+
+                    <div class="Undergraduate-Admissions-Profile-Card-Snippet-Card-Image" style="background-image:url('${cardObj.imageUrl}')">
+                    
+                    
+                    </div>
                         <div class="Undergraduate-Admissions-Profile-Card-Snippet-Card-Bottom">
                             <div class="Undergraduate-Admissions-Profile-Card-Snippet-Card-Bottom-Caption">
                                 <div class="Undergraduate-Admissions-Profile-Card-Snippet-Card-Bottom-Caption-Name">${cardObj.cardName}</div>
@@ -24,7 +29,57 @@ const createCardModel = (cardObj) => {
                                 <div class="Undergraduate-Admissions-Profile-Card-Snippet-Card-Overlay-Content-Name">${cardObj.cardName}</div>
                                 <div class="Undergraduate-Admissions-Profile-Card-Snippet-Card-Overlay-Content-Email">${cardObj.cardOverlayEmail}</div>
                                 <div class="Undergraduate-Admissions-Profile-Card-Snippet-Card-Overlay-Content-Body"><p>${cardObj.cardOverlayText}</p></div>
-                            </div>
+                                <div id="exit-div">
+                                <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                                viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
+                           <g>
+                               <g>
+                                   <path d="M256,0C114.508,0,0,114.497,0,256c0,141.493,114.497,256,256,256c141.492,0,256-114.497,256-256
+                                       C512,114.507,397.503,0,256,0z M256,472c-119.384,0-216-96.607-216-216c0-119.385,96.607-216,216-216
+                                       c119.384,0,216,96.607,216,216C472,375.385,375.393,472,256,472z"/>
+                               </g>
+                           </g>
+                           <g>
+                               <g>
+                                   <path d="M343.586,315.302L284.284,256l59.302-59.302c7.81-7.81,7.811-20.473,0.001-28.284c-7.812-7.811-20.475-7.81-28.284,0
+                                       L256,227.716l-59.303-59.302c-7.809-7.811-20.474-7.811-28.284,0c-7.81,7.811-7.81,20.474,0.001,28.284L227.716,256
+                                       l-59.302,59.302c-7.811,7.811-7.812,20.474-0.001,28.284c7.813,7.812,20.476,7.809,28.284,0L256,284.284l59.303,59.302
+                                       c7.808,7.81,20.473,7.811,28.284,0C351.398,335.775,351.397,323.112,343.586,315.302z"/>
+                               </g>
+                           </g>
+                           <g>
+                           </g>
+                           <g>
+                           </g>
+                           <g>
+                           </g>
+                           <g>
+                           </g>
+                           <g>
+                           </g>
+                           <g>
+                           </g>
+                           <g>
+                           </g>
+                           <g>
+                           </g>
+                           <g>
+                           </g>
+                           <g>
+                           </g>
+                           <g>
+                           </g>
+                           <g>
+                           </g>
+                           <g>
+                           </g>
+                           <g>
+                           </g>
+                           <g>
+                           </g>
+                           </svg>
+                             </div>
+                                                          </div>
                         </div>
     
     
--- src/js/index.js
+++ src/js/index.js
@@ -5,71 +5,71 @@ import createCardModel from './cardModel'
 
 
 
-// counselorOverlayArray.forEach( s => {
-//   cardMaker({
-//     imageUrl:s[0].trim(),
-//     cardName:s[1].trim(),
-//     cardFrontJob:s[2].trim(),
-//     cardFrontLocation:s[3].trim(),
-//     cardOverlayEmail:s[4].trim(),
-//     cardOverlayText:s[5].trim()
-//     })
+counselorOverlayArray.forEach( s => {
+  cardMaker({
+    imageUrl:s[0].trim(),
+    cardName:s[1].trim(),
+    cardFrontJob:s[2].trim(),
+    cardFrontLocation:s[3].trim(),
+    cardOverlayEmail:s[4].trim(),
+    cardOverlayText:s[5].trim()
+    })
+  })
+
+
+
+
+
+
+
+// cardMaker({
+//     imageUrl:'',
+//     cardName:'Marianna Angland',
+//     cardFrontJob:'Assistant Director of Enrollment Operations',
+//     cardFrontLocation:'New York',
+//     cardOverlayEmail:'enroll@stonybrook.edu',
+//     cardOverlayText:`Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates voluptatum quis, placeat quam necessitatibus, exercitationem quasi qui numquam accusantium nihil non ut, autem hic natus omnis dolore quidem dolorem. Blanditiis repellendus consectetur nostrum, aut cum eum reprehenderit fugiat tempora aliquam, suscipit in officiis omnis modi ratione non soluta eaque aspernatur temporibus pariatur eos quia eveniet.`
 //   })
-
-
-
-
-
-
-
-cardMaker({
-    imageUrl:'',
-    cardName:'Marianna Angland',
-    cardFrontJob:'Assistant Director of Enrollment Operations',
-    cardFrontLocation:'New York',
-    cardOverlayEmail:'enroll@stonybrook.edu',
-    cardOverlayText:`Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates voluptatum quis, placeat quam necessitatibus, exercitationem quasi qui numquam accusantium nihil non ut, autem hic natus omnis dolore quidem dolorem. Blanditiis repellendus consectetur nostrum, aut cum eum reprehenderit fugiat tempora aliquam, suscipit in officiis omnis modi ratione non soluta eaque aspernatur temporibus pariatur eos quia eveniet.`
-  })
-  cardMaker({
-    imageUrl:'https://cdn.vox-cdn.com/thumbor/ZbHT_HB51FBfJDn7mVkEOal3sHQ=/0x54:3047x2085/1200x800/filters:focal(0x54:3047x2085)/cdn.vox-cdn.com/uploads/chorus_image/image/30109923/20130215_mje_aw8_487.0.jpg',
-    cardName:'Tom Jerry',
-    cardFrontJob:'Assistant Director of Enrollment Operations',
-    cardFrontLocation:'New York',
-    cardOverlayEmail:'enroll@stonybrook.edu',
-    cardOverlayText:`Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates voluptatum quis, placeat quam necessitatibus, exercitationem quasi qui numquam accusantium nihil non ut, autem hic natus omnis dolore quidem dolorem. Blanditiis repellendus consectetur nostrum, aut cum eum reprehenderit fugiat tempora aliquam, suscipit in officiis omnis modi ratione non soluta eaque aspernatur temporibus pariatur eos quia eveniet.`
-  })
-  cardMaker({
-    imageUrl:'https://cdn.vox-cdn.com/thumbor/ZbHT_HB51FBfJDn7mVkEOal3sHQ=/0x54:3047x2085/1200x800/filters:focal(0x54:3047x2085)/cdn.vox-cdn.com/uploads/chorus_image/image/30109923/20130215_mje_aw8_487.0.jpg',
-    cardName:'Bruce Bruce',
-    cardFrontJob:'Assistant Director of Enrollment Operations',
-    cardFrontLocation:'New York',
-    cardOverlayEmail:'enroll@stonybrook.edu',
-    cardOverlayText:`Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates voluptatum quis, placeat quam necessitatibus, exercitationem quasi qui numquam accusantium nihil non ut, autem hic natus omnis dolore quidem dolorem. Blanditiis repellendus consectetur nostrum, aut cum eum reprehenderit fugiat tempora aliquam, suscipit in officiis omnis modi ratione non soluta eaque aspernatur temporibus pariatur eos quia eveniet.`
-  })
-  cardMaker({
-    imageUrl:'https://cdn.vox-cdn.com/thumbor/ZbHT_HB51FBfJDn7mVkEOal3sHQ=/0x54:3047x2085/1200x800/filters:focal(0x54:3047x2085)/cdn.vox-cdn.com/uploads/chorus_image/image/30109923/20130215_mje_aw8_487.0.jpg',
-    cardName:'Jim Ang',
-    cardFrontJob:'Assistant Director of Enrollment Operations',
-    cardFrontLocation:'New York',
-    cardOverlayEmail:'enroll@stonybrook.edu',
-    cardOverlayText:`Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates voluptatum quis, placeat quam necessitatibus, exercitationem quasi qui numquam accusantium nihil non ut, autem hic natus omnis dolore quidem dolorem. Blanditiis repellendus consectetur nostrum, aut cum eum reprehenderit fugiat tempora aliquam, suscipit in officiis omnis modi ratione non soluta eaque aspernatur temporibus pariatur eos quia eveniet.`
-  })
-  cardMaker({
-    imageUrl:'',
-    cardName:'Vin Angland',
-    cardFrontJob:'Assistant Director of Enrollment Operations',
-    cardFrontLocation:'New York',
-    cardOverlayEmail:'enroll@stonybrook.edu',
-    cardOverlayText:`Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates voluptatum quis, placeat quam necessitatibus, exercitationem quasi qui numquam accusantium nihil non ut, autem hic natus omnis dolore quidem dolorem. Blanditiis repellendus consectetur nostrum, aut cum eum reprehenderit fugiat tempora aliquam, suscipit in officiis omnis modi ratione non soluta eaque aspernatur temporibus pariatur eos quia eveniet.`
-  })
-  cardMaker({
-    imageUrl:'https://cdn.vox-cdn.com/thumbor/ZbHT_HB51FBfJDn7mVkEOal3sHQ=/0x54:3047x2085/1200x800/filters:focal(0x54:3047x2085)/cdn.vox-cdn.com/uploads/chorus_image/image/30109923/20130215_mje_aw8_487.0.jpg',
-    cardName:'Bob Bobs',
-    cardFrontJob:'Assistant Director of Enrollment Operations',
-    cardFrontLocation:'New York',
-    cardOverlayEmail:'enroll@stonybrook.edu',
-    cardOverlayText:`Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates voluptatum quis, placeat quam necessitatibus, exercitationem quasi qui numquam accusantium nihil non ut, autem hic natus omnis dolore quidem dolorem. Blanditiis repellendus consectetur nostrum, aut cum eum reprehenderit fugiat tempora aliquam, suscipit in officiis omnis modi ratione non soluta eaque aspernatur temporibus pariatur eos quia eveniet.`
-  })
+//   cardMaker({
+//     imageUrl:'https://cdn.vox-cdn.com/thumbor/ZbHT_HB51FBfJDn7mVkEOal3sHQ=/0x54:3047x2085/1200x800/filters:focal(0x54:3047x2085)/cdn.vox-cdn.com/uploads/chorus_image/image/30109923/20130215_mje_aw8_487.0.jpg',
+//     cardName:'Tom Jerry',
+//     cardFrontJob:'Assistant Director of Enrollment Operations',
+//     cardFrontLocation:'New York',
+//     cardOverlayEmail:'enroll@stonybrook.edu',
+//     cardOverlayText:`Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates voluptatum quis, placeat quam necessitatibus, exercitationem quasi qui numquam accusantium nihil non ut, autem hic natus omnis dolore quidem dolorem. Blanditiis repellendus consectetur nostrum, aut cum eum reprehenderit fugiat tempora aliquam, suscipit in officiis omnis modi ratione non soluta eaque aspernatur temporibus pariatur eos quia eveniet.`
+//   })
+//   cardMaker({
+//     imageUrl:'https://cdn.vox-cdn.com/thumbor/ZbHT_HB51FBfJDn7mVkEOal3sHQ=/0x54:3047x2085/1200x800/filters:focal(0x54:3047x2085)/cdn.vox-cdn.com/uploads/chorus_image/image/30109923/20130215_mje_aw8_487.0.jpg',
+//     cardName:'Bruce Bruce',
+//     cardFrontJob:'Assistant Director of Enrollment Operations',
+//     cardFrontLocation:'New York',
+//     cardOverlayEmail:'enroll@stonybrook.edu',
+//     cardOverlayText:`Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates voluptatum quis, placeat quam necessitatibus, exercitationem quasi qui numquam accusantium nihil non ut, autem hic natus omnis dolore quidem dolorem. Blanditiis repellendus consectetur nostrum, aut cum eum reprehenderit fugiat tempora aliquam, suscipit in officiis omnis modi ratione non soluta eaque aspernatur temporibus pariatur eos quia eveniet.`
+//   })
+//   cardMaker({
+//     imageUrl:'https://cdn.vox-cdn.com/thumbor/ZbHT_HB51FBfJDn7mVkEOal3sHQ=/0x54:3047x2085/1200x800/filters:focal(0x54:3047x2085)/cdn.vox-cdn.com/uploads/chorus_image/image/30109923/20130215_mje_aw8_487.0.jpg',
+//     cardName:'Jim Ang',
+//     cardFrontJob:'Assistant Director of Enrollment Operations',
+//     cardFrontLocation:'New York',
+//     cardOverlayEmail:'enroll@stonybrook.edu',
+//     cardOverlayText:`Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates voluptatum quis, placeat quam necessitatibus, exercitationem quasi qui numquam accusantium nihil non ut, autem hic natus omnis dolore quidem dolorem. Blanditiis repellendus consectetur nostrum, aut cum eum reprehenderit fugiat tempora aliquam, suscipit in officiis omnis modi ratione non soluta eaque aspernatur temporibus pariatur eos quia eveniet.`
+//   })
+//   cardMaker({
+//     imageUrl:'',
+//     cardName:'Vin Angland',
+//     cardFrontJob:'Assistant Director of Enrollment Operations',
+//     cardFrontLocation:'New York',
+//     cardOverlayEmail:'enroll@stonybrook.edu',
+//     cardOverlayText:`Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates voluptatum quis, placeat quam necessitatibus, exercitationem quasi qui numquam accusantium nihil non ut, autem hic natus omnis dolore quidem dolorem. Blanditiis repellendus consectetur nostrum, aut cum eum reprehenderit fugiat tempora aliquam, suscipit in officiis omnis modi ratione non soluta eaque aspernatur temporibus pariatur eos quia eveniet.`
+//   })
+//   cardMaker({
+//     imageUrl:'https://cdn.vox-cdn.com/thumbor/ZbHT_HB51FBfJDn7mVkEOal3sHQ=/0x54:3047x2085/1200x800/filters:focal(0x54:3047x2085)/cdn.vox-cdn.com/uploads/chorus_image/image/30109923/20130215_mje_aw8_487.0.jpg',
+//     cardName:'Bob Bobs',
+//     cardFrontJob:'Assistant Director of Enrollment Operations',
+//     cardFrontLocation:'New York',
+//     cardOverlayEmail:'enroll@stonybrook.edu',
+//     cardOverlayText:`Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates voluptatum quis, placeat quam necessitatibus, exercitationem quasi qui numquam accusantium nihil non ut, autem hic natus omnis dolore quidem dolorem. Blanditiis repellendus consectetur nostrum, aut cum eum reprehenderit fugiat tempora aliquam, suscipit in officiis omnis modi ratione non soluta eaque aspernatur temporibus pariatur eos quia eveniet.`
+//   })
 
 
 
@@ -97,7 +97,7 @@ cardMaker({
 let cardWrap = document.querySelectorAll('.Undergraduate-Admissions-Profile-Card-Snippet-CardWrapper')
 let overlayWrap = document.querySelectorAll('.Undergraduate-Admissions-Profile-Card-Snippet-Card-Overlay')
 let back = document.querySelectorAll('.Undergraduate-Admissions-Profile-Card-Snippet-Card-Overlay-Wrap')
-
+const exitButton = document.querySelectorAll('#exit-div')
 
 cardWrap
 .forEach((item,index) => {
@@ -108,7 +108,15 @@ cardWrap
           back[index].classList.remove('active')
           overlayWrap[index].classList.remove('active')
          })
+  })
+})
 
-  
+
+exitButton
+.forEach((item,index) => {
+  item.addEventListener('click', event => {
+          back[index].classList.remove('active')
+          overlayWrap[index].classList.remove('active')
+         
   })
 })
